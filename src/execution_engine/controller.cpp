5c1370b9c5370574a75f1f97b5253b5aef0affb3
thraneh <thraneh@gmail.com>
2017-12-01 17:33:51 +0100

    fix execution engine with new flatbuffers interface

--- src/execution_engine/controller.cpp
+++ src/execution_engine/controller.cpp
@@ -39,44 +39,116 @@ void Controller::on_read() {
         data = _buffer.pullup(LENGTH_ENVELOPE + length_payload);
         if (data) {
             data += LENGTH_ENVELOPE;
+            // FIXME(thraneha): remember envelope.type disappears !!!
             try {
-                auto type = envelope.type();
+                const auto root = flatbuffers::GetRoot<schema::Event>(data);
+                const auto message_info = common::convert(root->message_info());
+                const auto type = root->event_data_type();
                 switch (type) {
-                    case common::MessageType::GatewayStatusEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::GatewayStatusEvent>(data)));
+                    case schema::EventData::GatewayStatus:
+                            {
+                            const auto gateway_status = common::convert(root->event_data_as_GatewayStatus());
+                            const auto event = common::GatewayStatusEvent{
+                                .message_info = message_info,
+                                .gateway_status = gateway_status};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::ReferenceDataEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::ReferenceDataEvent>(data)));
+                    case schema::EventData::ReferenceData:
+                            {
+                            const auto reference_data = common::convert(root->event_data_as_ReferenceData());
+                            const auto event = common::ReferenceDataEvent{
+                                .message_info = message_info,
+                                .reference_data = reference_data};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::MarketStatusEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::MarketStatusEvent>(data)));
+                    case schema::EventData::MarketStatus:
+                            {
+                            const auto market_status = common::convert(root->event_data_as_MarketStatus());
+                            const auto event = common::MarketStatusEvent{
+                                .message_info = message_info,
+                                .market_status = market_status};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::MarketByPriceEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::MarketByPriceEvent>(data)));
+                    case schema::EventData::MarketByPrice:
+                            {
+                            const auto market_by_price = common::convert(root->event_data_as_MarketByPrice());
+                            const auto event = common::MarketByPriceEvent{
+                                .message_info = message_info,
+                                .market_by_price = market_by_price};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::SessionStatisticsEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::SessionStatisticsEvent>(data)));
+                    case schema::EventData::SessionStatistics:
+                            {
+                            const auto session_statistics = common::convert(root->event_data_as_SessionStatistics());
+                            const auto event = common::SessionStatisticsEvent{
+                                .message_info = message_info,
+                                .session_statistics = session_statistics};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::DailyStatisticsEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::DailyStatisticsEvent>(data)));
+                    case schema::EventData::DailyStatistics:
+                            {
+                            const auto daily_statistics = common::convert(root->event_data_as_DailyStatistics());
+                            const auto event = common::DailyStatisticsEvent{
+                                .message_info = message_info,
+                                .daily_statistics = daily_statistics};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::CreateOrderAckEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::CreateOrderAckEvent>(data)));
+                    case schema::EventData::CreateOrderAck:
+                            {
+                            const auto create_order_ack = common::convert(root->event_data_as_CreateOrderAck());
+                            const auto event = common::CreateOrderAckEvent{
+                                .message_info = message_info,
+                                .create_order_ack = create_order_ack};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::ModifyOrderAckEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::ModifyOrderAckEvent>(data)));
+                    case schema::EventData::ModifyOrderAck:
+                            {
+                            const auto modify_order_ack = common::convert(root->event_data_as_ModifyOrderAck());
+                            const auto event = common::ModifyOrderAckEvent{
+                                .message_info = message_info,
+                                .modify_order_ack = modify_order_ack};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::CancelOrderAckEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::CancelOrderAckEvent>(data)));
+                    case schema::EventData::CancelOrderAck:
+                            {
+                            const auto cancel_order_ack = common::convert(root->event_data_as_CancelOrderAck());
+                            const auto event = common::CancelOrderAckEvent{
+                                .message_info = message_info,
+                                .cancel_order_ack = cancel_order_ack};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::OrderUpdateEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::OrderUpdateEvent>(data)));
+                    case schema::EventData::OrderUpdate:
+                            {
+                            const auto order_update = common::convert(root->event_data_as_OrderUpdate());
+                            const auto event = common::OrderUpdateEvent{
+                                .message_info = message_info,
+                                .order_update = order_update};
+                            _strategy.on(event);
+                            }
                         break;
-                    case common::MessageType::TradeUpdateEvent:
-                        _strategy.on(common::convert(flatbuffers::GetRoot<schema::TradeUpdateEvent>(data)));
+                    case schema::EventData::TradeUpdate:
+                            {
+                            const auto trade_update = common::convert(root->event_data_as_TradeUpdate());
+                            const auto event = common::TradeUpdateEvent{
+                                .message_info = message_info,
+                                .trade_update = trade_update};
+                            _strategy.on(event);
+                            }
                         break;
                     default:
-                        LOG(WARNING) << "Unknown message type: " << static_cast<uint8_t>(type);
+                        if (schema::EventData::MAX < type)
+                            LOG(WARNING) << "Unknown message type: " << static_cast<uint8_t>(type);
+                        else
+                            LOG(WARNING) << "Unknown message type: " << schema::EnumNamesEventData()[static_cast<uint8_t>(type)];
                 }
             }
             catch (...) {

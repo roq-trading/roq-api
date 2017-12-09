4e8c9948bad7d957fcb8411a42b4be27c1aed181
thraneh <thraneh@gmail.com>
2017-12-09 08:52:03 +0100

    switching to autotools for libevent (cmake didn't install libevent_pthreads)

--- scripts/install-libevent.sh
+++ scripts/install-libevent.sh
@@ -3,4 +3,7 @@ set -ex
 VERSION="release-2.1.8-stable"
 wget https://github.com/libevent/libevent/archive/$VERSION.tar.gz
 tar -xzvf $VERSION.tar.gz
-cd libevent-$VERSION && cmake -DCMAKE_BUILD_TYPE=Release . && make -j2 && sudo make install
+cd libevent-$VERSION && ./autogen.sh && ./configure && make -j2 && sudo make install
+
+# CMAKE doesn't install libevent_pthreads
+# cd libevent-$VERSION && cmake -DCMAKE_BUILD_TYPE=Release . && make -j2 && sudo make install

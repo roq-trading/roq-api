5aa7e90c74baa28a6ab69255d303b317392a5663
thraneh <thraneh@gmail.com>
2017-12-09 08:30:51 +0100

    it's getting a bit tiresome ... now trying with explicitly replacing gcc

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
+sudo apt-get install -qq g++-6
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
 sudo apt-get install -y \
 	libssl-dev build-essential m4 autoconf autoconf-archive automake \
        	libtool pkg-config cmake gcc libgoogle-glog-dev libconfig++-dev

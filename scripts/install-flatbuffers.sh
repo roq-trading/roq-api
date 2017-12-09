05680be73b06c2206b63fab05b718bb89926298e
thraneh <thraneh@gmail.com>
2017-12-09 07:33:36 +0100

    flatbuffers needs release build before it install flatc...

--- scripts/install-flatbuffers.sh
+++ scripts/install-flatbuffers.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://github.com/google/flatbuffers/archive/v1.8.0.tar.gz
 tar -xzvf v1.8.0.tar.gz
-cd flatbuffers-1.8.0 && cmake . && make -j2 && sudo make install
+cd flatbuffers-1.8.0 && cmake -DCMAKE_BUILD_TYPE=Release . && make -j2 && sudo make install

b913e004be4c280eded5aba7633ab9d17ff44d1a
thraneh <thraneh@gmail.com>
2017-12-18 13:32:46 +0100

    separate the client controller + make example more intuitive to read

---
+++ examples/trading_engine/controller.h
@@ -0,0 +1,165 @@
+/* Copyright (c) 2017,  Hans Erik Thrane */
+
+#pragma once
+
+#include <glog/logging.h>
+
+#include <quinclas/tradingapi.h>
+#include <quinclas/io/libevent.h>
+
+#include <algorithm>
+#include <iomanip>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace examples {
+namespace trading_engine {
+
+template <typename T>
+class Controller final {
+  typedef std::unordered_map<std::string, std::string> gateways_t;
+
+ public:
+  explicit Controller(const gateways_t&& gateways) : _gateways(std::move(gateways)) {}
+  template <typename... Args>
+  void create_and_dispatch(Args&&... args) {
+    Dispatcher(_gateways, std::forward<Args>(args)...).dispatch();
+  }
+
+ private:
+  class Dispatcher final
+      : public quinclas::common::Strategy::Dispatcher,
+        public quinclas::io::libevent::TimerEvent::Handler {
+    // managing the state of gateway connectivity
+    class Gateway final {
+     public:
+      Gateway(const std::string& name, const int domain, const std::string& address,
+              quinclas::io::libevent::Base& base, std::unordered_set<Gateway *>& callbacks)
+          : _name(name), _domain(domain), _address(address), _base(base), _callbacks(callbacks), _state(Disconnected),
+            _retry(0), _countdown(0) {}
+      bool refresh() {
+        if (0 < _countdown && 0 != --_countdown)
+          return false;
+        switch (_state) {
+          case Disconnected: {
+            LOG(INFO) << "gateway name=" << _name << ", state=connecting, retry=" << _retry;
+            ++_retry;
+            _state = Connecting;
+            connect();
+            return true;  // remove
+          }
+          case Failed: {
+            LOG(INFO) << "gateway name=" << _name << ", state=disconnected";
+            _buffer_event.release();
+            _state = Disconnected;
+            _countdown = std::min(std::max(_retry, 1), 10);  // TODO(thraneh): table lookup
+            _callbacks.insert(this);
+            return false;  // keep
+          }
+          default:
+            assert(false);  // should never get here...
+        }
+      }
+
+     private:
+      void connect() {
+        quinclas::io::net::Socket socket(_domain, SOCK_STREAM, 0);
+        socket.non_blocking(true);
+        auto buffer_event = std::unique_ptr<quinclas::io::libevent::BufferEvent>(
+          new quinclas::io::libevent::BufferEvent(_base, std::move(socket)));
+        // TODO(thraneh): c+11 lambda for callback
+        buffer_event->setcb(on_read, nullptr, on_error, this);
+        buffer_event->enable(EV_READ);
+        buffer_event->connect(_address);
+        _buffer_event = std::move(buffer_event);
+      }
+      static void on_read(struct bufferevent *bev, void *arg) {
+        // decode and notify strategy -- should we take strategy as arg to constructor?
+        LOG(INFO) << "read";
+      }
+      static void on_error(struct bufferevent *bev, short what, void *arg) {  // NOLINT short
+        // TODO(thraneh): move self to zombie state
+        auto& self = *reinterpret_cast<Gateway *>(arg);
+        LOG(INFO) << "gateway name=" << self._name << ", error=0x" << std::setw(2) << std::hex << what;
+        if (what & BEV_EVENT_CONNECTED) {
+          LOG(INFO) << "gateway name=" << self._name << ", state=connected";
+          self._state = Connected;
+          self._retry = 0;
+        } else {  // unless we start using the timeout feature, this should catch all other errors
+          LOG(INFO) << "gateway name=" << self._name << ", state=failed";
+          self._state = Failed;
+          self._callbacks.insert(&self);
+        }
+      }
+
+     private:
+      std::string _name;
+      int _domain;
+      quinclas::io::net::Address _address;
+      quinclas::io::libevent::Base& _base;
+      std::unordered_set<Gateway *>& _callbacks;
+      enum { Disconnected, Connecting, Connected, Failed } _state;
+      int _retry;
+      int _countdown;
+      std::unique_ptr<quinclas::io::libevent::BufferEvent> _buffer_event;
+    };
+
+   public:
+    template <typename... Args>
+    explicit Dispatcher(const gateways_t& gateways, Args&&... args)
+        : _strategy(*this, std::forward<Args>(args)...),  // request handler, then whatever the strategy needs
+          _timer(*this, _base) {
+      // TODO(thraneh): check duplicate address
+      for (const auto iter : gateways) {
+        // TODO(thraneh): C++11 doesn't return the reference -- C++17 can fix next 2 lines (or just use abseil ?)
+        _gateways.emplace_back(Gateway(iter.first, PF_LOCAL, iter.second, _base, _callbacks));
+        Gateway *gateway = &(*_gateways.rbegin());
+        _gateways_by_name[iter.first] = gateway;
+        _callbacks.insert(gateway);
+      }
+    }
+    void dispatch() {
+      _timer.add({.tv_sec = 1, .tv_usec = 0 });
+      _base.loop(EVLOOP_NO_EXIT_ON_EMPTY);
+    }
+
+   private:
+    void send(const quinclas::common::CreateOrderRequest& create_order_request) override {
+      Gateway *gateway = _gateways_by_name[create_order_request.request_info.destination];
+      // TODO(thraneh): implement
+    }
+    void send(const quinclas::common::ModifyOrderRequest& modify_order_request) override {
+      Gateway *gateway = _gateways_by_name[modify_order_request.request_info.destination];
+      // TODO(thraneh): implement
+    }
+    void send(const quinclas::common::CancelOrderRequest& cancel_order_request) override {
+      Gateway *gateway = _gateways_by_name[cancel_order_request.request_info.destination];
+      // TODO(thraneh): implement
+    }
+    void on_timer() override {
+      std::list<Gateway *> remove;
+      for (const auto iter : _callbacks)
+        if ((*iter).refresh())
+          remove.push_back(iter);
+      for (auto iter : remove)
+        _callbacks.erase(iter);
+    }
+
+   private:
+    T _strategy;
+    quinclas::io::libevent::Base _base;
+    quinclas::io::libevent::TimerEvent _timer;
+    std::vector<Gateway> _gateways;
+    std::unordered_map<std::string, Gateway *> _gateways_by_name;
+    std::unordered_set<Gateway *> _callbacks;
+  };
+
+ private:
+  gateways_t _gateways;
+};
+
+}  // namespace trading_engine
+}  // namespace examples
--- examples/trading_engine/main.cpp
+++ examples/trading_engine/main.cpp
@@ -3,135 +3,41 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
-#include <quinclas/io/libevent.h>
-
-#include <list>
-#include <string>
-#include <unordered_map>
+// currently in this directory  -- will be moved into quinclas/tradingapi when feature complete
+#include "trading_engine/controller.h"
 
 #include "your/strategy.h"
 
 DEFINE_string(local_address, "", "host-internal socket address (path)");
 
-// FIXME(thraneh): WIP -- something closer to a real strategy dispatch interface.
-namespace {
-template <typename T>
-class Controller final {
-  typedef std::unordered_map<std::string, std::string> gateways_t;
-
- public:
-  explicit Controller(const gateways_t&& gateways) : _gateways(std::move(gateways)) {}
-  template <typename... Args>
-  void create_and_dispatch(Args&&... args) {
-    Dispatcher(_gateways, std::forward<Args>(args)...).dispatch();
-  }
-
- private:
-  class Dispatcher final
-      : public quinclas::common::Strategy::Dispatcher,
-        public quinclas::io::libevent::TimerEvent::Handler {
-   public:
-    template <typename... Args>
-    explicit Dispatcher(const gateways_t& gateways, Args&&... args)
-        : _strategy(*this, std::forward<Args>(args)...),  // first, the request handler, then whatever you need
-          _timer(*this, _base) {
-      // TODO(thraneh): check duplicate address
-      for (const auto iter : gateways) {
-        auto res = _gateways.emplace(iter.first, Gateway(PF_LOCAL, iter.second));
-        if (res.second)
-          _disconnected.push_back((*res.first).first);
-      }
-    }
-    void dispatch() {
-      _timer.add({.tv_sec = 1, .tv_usec = 0 });
-      _base.loop(EVLOOP_NO_EXIT_ON_EMPTY);
-    }
-
-   private:
-    void send(const quinclas::common::CreateOrderRequest& create_order_request) override {}
-    void send(const quinclas::common::ModifyOrderRequest& modify_order_request) override {}
-    void send(const quinclas::common::CancelOrderRequest& cancel_order_request) override {}
-    void on_timer() override {
-      LOG(INFO) << "timer";
-      for (const auto iter : _gateways) {
-        auto socket = iter.second.create_socket();
-        _connecting.push_back(std::unique_ptr<Connection>(
-              new Connection(_base, std::move(socket), iter.second._address)));
-      }
-    }
-
-   private:
-    enum State {
-      Disconnected,
-      Connecting,
-      Connected,
-      Zombie
-    };
-    struct Gateway final {
-      Gateway(const int domain, const std::string& address) : _domain(domain), _address(address) {}
-      quinclas::io::net::Socket create_socket() const {
-        quinclas::io::net::Socket socket(_domain, SOCK_STREAM, 0);
-        socket.non_blocking(true);
-        return socket;
-      }
-      int _domain;
-      quinclas::io::net::Address _address;
-    };
-    struct Connection final {
-      explicit Connection(quinclas::io::libevent::Base& base, quinclas::io::net::Socket socket,
-                          const quinclas::io::net::Address& address)
-          : _buffer_event(base, std::move(socket)) {
-        _buffer_event.setcb(on_read, nullptr, on_error, this);
-        _buffer_event.enable(EV_READ);
-        _buffer_event.connect(address);
-        // FIXME(thraneh): move to member
-        // change state from disconnected to connecting
-      }
-
-     private:
-      static void on_read(struct bufferevent *bev, void *arg) {
-        // decode and notify strategy -- should we take strategy as arg to constructor?
-      }
-      static void on_error(struct bufferevent *bev, short what, void *arg) {  // NOLINT short
-        // TODO(thraneh): move self to zombie state
-      }
-
-     private:
-      quinclas::io::libevent::BufferEvent _buffer_event;
-    };
-
-   private:
-    T _strategy;
-    std::unordered_map<std::string, Gateway> _gateways;
-    quinclas::io::libevent::Base _base;
-    quinclas::io::libevent::TimerEvent _timer;
-    // state management
-    std::list<std::string> _disconnected;
-    std::list<std::unique_ptr<Connection> > _connecting;
-  };
-
- private:
-  gateways_t _gateways;
-};
-}  // namespace
-
 int main(int argc, char *argv[]) {
   // initialize logging library
+
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
+
   // parse command-line options
+
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   gflags::ShutDownCommandLineFlags();
+
   // validate command-line options
-  if (FLAGS_local_address.empty())
-    throw std::invalid_argument("local_address is missing");
-  // ready
+
+  if (FLAGS_local_address.empty()) {
+    LOG(ERROR) << "local-address is a required parameter";
+    return EXIT_FAILURE;
+  }
+
   LOG(INFO) << "===== START =====";
+
   // create framework, instantiate strategy and start event dispatching
-  Controller<examples::your::Strategy>({
+
+  examples::trading_engine::Controller<examples::your::Strategy>({
       { "FEMAS", FLAGS_local_address },
-    }).create_and_dispatch(10);
-  // done
+    }).create_and_dispatch(
+      /*ticks_to_trade =*/ 10);
+
   LOG(INFO) << "===== STOP =====";
-  return EXIT_SUCCESS;  // TODO(thraneh): event-loop should be able to stop with an error code
+
+  return EXIT_SUCCESS;
 }

9aeca7d7a9dab332a67e58fc2099a5d5eefde688
thraneh <thraneh@gmail.com>
2018-01-30 08:54:54 +0100

    incorporating exposure management

--- examples/reference/order_manager.cpp
+++ examples/reference/order_manager.cpp
@@ -2,6 +2,7 @@
 
 #include "reference/order_manager.h"
 #include <glog/logging.h>
+#include <algorithm>
 #include <limits>
 #include "reference/utilities.h"
 
@@ -9,13 +10,129 @@ namespace examples {
 namespace reference {
 
 const char *GATEWAY = "FEMAS";
-const char *IOC = "ioc";
 
 // constructor
 
 OrderManager::OrderManager(const Config& config, const RiskManager& risk_manager,
                            quinclas::common::Strategy::Dispatcher& dispatcher)
     : _config(config), _risk_manager(risk_manager), _dispatcher(dispatcher) {}
+
+
+// create order
+
+uint32_t OrderManager::buy(const char *order_template, double quantity,
+                           double limit_price) {
+  return create_order(order_template, quinclas::common::TradeDirection::Buy,
+                      quantity, limit_price);
+}
+
+uint32_t OrderManager::sell(const char *order_template, double quantity,
+                            double limit_price) {
+  return create_order(order_template, quinclas::common::TradeDirection::Sell,
+                      quantity, limit_price);
+}
+
+uint32_t OrderManager::create_order(const char *order_template,
+                                    quinclas::common::TradeDirection direction,
+                                    double quantity, double limit_price) {
+  // risk manager
+  // TODO(thraneh): here we need to access a global "time" !!!
+  double exposure = get_exposure(direction);
+  if (!_risk_manager.can_trade(_config.instrument, direction,
+                               quantity, exposure))
+    return 0;
+  // create order
+  auto order_id = ++_next_order_id;
+  try {
+    quinclas::common::CreateOrder create_order {
+      .opaque         = order_id,
+      .order_template = order_template,
+      .exchange       = _config.exchange.c_str(),
+      .instrument     = _config.instrument.c_str(),
+      .direction      = direction,
+      .quantity       = quantity,
+      .limit_price    = limit_price,
+      .stop_price     = std::numeric_limits<double>::quiet_NaN(),
+    };
+    _dispatcher.send(GATEWAY, create_order);
+  } catch (std::exception& e) {  // TODO(thraneh): more specific type(s)
+    LOG(WARNING) << e.what();
+    return 0;
+  }
+  _orders.emplace(order_id, Order(direction, quantity));
+  _timeout.emplace_back(_last_update_time + _config.order_timeout, order_id);
+  update_exposure(direction, quantity);
+  return order_id;
+}
+
+void OrderManager::update_order(uint32_t order_id, uint32_t gateway_order_id,
+                                double remaining_quantity) {
+  auto iter = _orders.find(order_id);
+  if (iter == _orders.end())
+    return;
+  auto& order = (*iter).second;
+  order.gateway_order_id = gateway_order_id;
+  auto delta_quantity = remaining_quantity - order.remaining_quantity;
+  if (is_equal(delta_quantity, 0.0))
+    return;
+  order.remaining_quantity = remaining_quantity;
+  update_exposure(order.direction, delta_quantity);
+}
+
+void OrderManager::remove_order(uint32_t order_id) {
+  auto iter = _orders.find(order_id);
+  if (iter == _orders.end())
+    return;
+  auto& order = (*iter).second;
+  update_exposure(order.direction, -order.remaining_quantity);
+  _orders.erase(iter);
+}
+
+double OrderManager::get_exposure(
+    quinclas::common::TradeDirection direction) const {
+  switch (direction) {
+    case quinclas::common::TradeDirection::Undefined:
+      LOG(FATAL) << "Unexpected direction";
+    case quinclas::common::TradeDirection::Buy:
+      return _buy_exposure;
+    case quinclas::common::TradeDirection::Sell:
+      return _sell_exposure;
+  }
+}
+
+void OrderManager::update_exposure(quinclas::common::TradeDirection direction,
+                                   double delta_quantity) {
+  switch (direction) {
+    case quinclas::common::TradeDirection::Undefined:
+      LOG(FATAL) << "Unexpected direction";
+    case quinclas::common::TradeDirection::Buy:
+      _buy_exposure = std::max(_buy_exposure + delta_quantity, 0.0);
+      break;
+    case quinclas::common::TradeDirection::Sell:
+      _sell_exposure = std::max(_sell_exposure + delta_quantity, 0.0);
+      break;
+  }
+}
+
+// slow -- use only for testing and debug
+void OrderManager::compute_and_update_exposure() {
+  double buy_exposure = 0.0, sell_exposure = 0.0;
+  for (const auto iter : _orders) {
+    const auto& order = iter.second;
+    switch (order.direction) {
+      case quinclas::common::TradeDirection::Undefined:
+        LOG(FATAL) << "Unexpected direction";
+      case quinclas::common::TradeDirection::Buy:
+        buy_exposure += order.remaining_quantity;
+        break;
+      case quinclas::common::TradeDirection::Sell:
+        sell_exposure += order.remaining_quantity;
+        break;
+    }
+  }
+  _buy_exposure = buy_exposure;
+  _sell_exposure = sell_exposure;
+}
 
 // event handlers
 
@@ -27,12 +144,12 @@ void OrderManager::on(const quinclas::common::CreateOrderAckEvent& event) {
   check(event.message_info);
   const auto& create_order_ack = event.create_order_ack;
   auto order_id = create_order_ack.opaque;
-  auto iter = _orders.find(order_id);
-  if (iter == _orders.end())
-    return;
   if (create_order_ack.failure) {
-    _orders.erase(iter);
+    remove_order(order_id);
   } else {
+    auto iter = _orders.find(order_id);
+    if (iter == _orders.end())
+      return;
     auto& order = (*iter).second;
     order.gateway_order_id = create_order_ack.order_id;
   }
@@ -40,12 +157,28 @@ void OrderManager::on(const quinclas::common::CreateOrderAckEvent& event) {
 
 void OrderManager::on(const quinclas::common::ModifyOrderAckEvent& event) {
   check(event.message_info);
-  // TODO(thraneh): implement
+  const auto& modify_order_ack = event.modify_order_ack;
+  auto order_id = modify_order_ack.opaque;
+  if (modify_order_ack.failure) {
+    LOG(WARNING) << "Failed to modify the order!";
+  } else {
+    auto iter = _orders.find(order_id);
+    if (iter == _orders.end())
+      return;
+    auto& order = (*iter).second;
+    // TODO(thraneh): this feels unsafe
+    update_exposure(order.direction, modify_order_ack.quantity_change);
+  }
 }
 
 void OrderManager::on(const quinclas::common::CancelOrderAckEvent& event) {
   check(event.message_info);
-  // TODO(thraneh): implement
+  const auto& cancel_order_ack = event.cancel_order_ack;
+  auto order_id = cancel_order_ack.opaque;
+  if (cancel_order_ack.failure)
+    LOG(WARNING) << "Failed to cancel the order!";
+  else
+    remove_order(order_id);
 }
 
 void OrderManager::on(const quinclas::common::OrderUpdateEvent& event) {
@@ -84,78 +217,29 @@ void OrderManager::on(const quinclas::common::OrderUpdateEvent& event) {
       remove = is_equal(order.remaining_quantity, 0.0);
       break;
   }
-  if (remove)
-    _orders.erase(iter);
-  else if (timer)
-    _timeout.push_back(std::make_pair(_last_update_time + _config.order_timeout, order_id));
-  // TODO(thraneh): update exposure
-}
-
-// create order
-
-bool OrderManager::buy_ioc(double quantity, double limit_price) {
-  return create_order(IOC, quinclas::common::TradeDirection::Buy,
-                      quantity, limit_price);
-}
-
-bool OrderManager::sell_ioc(double quantity, double limit_price) {
-  return create_order(IOC, quinclas::common::TradeDirection::Sell,
-                      quantity, limit_price);
-}
-
-bool OrderManager::create_order(const char *order_template,
-                                quinclas::common::TradeDirection direction,
-                                double quantity, double limit_price) {
-  // TODO(thraneh): we need a global "time" !!!
-  // risk manager must confirm
-  double exposure = get_exposure(direction);
-  if (!_risk_manager.can_trade(_config.instrument, direction,
-                               quantity, exposure))
-    return false;
-  // send the new order
-  try {
-    quinclas::common::CreateOrder create_order {
-      .opaque         = 0,
-      .order_template = order_template,
-      .exchange       = _config.exchange.c_str(),
-      .instrument     = _config.instrument.c_str(),
-      .direction      = direction,
-      .quantity       = quantity,
-      .limit_price    = limit_price,
-      .stop_price     = std::numeric_limits<double>::quiet_NaN(),
-    };
-    _dispatcher.send(GATEWAY, create_order);
-  } catch (std::exception& e) {
-    LOG(WARNING) << e.what();
-    return false;
+  if (remove) {
+    remove_order(order_id);
+  } else {
+    if (timer)
+      _timeout.emplace_back(_last_update_time + _config.order_timeout, order_id);
+    // TODO(thraneh): this feels unsafe
+    auto delta_quantity = order_update.remaining_quantity
+                        - order.remaining_quantity;
+    update_exposure(order.direction, delta_quantity);
   }
-  // TODO(thraneh): add to exposure
-  auto order_id = ++_next_internal_id;
-  _orders.emplace(order_id, Order(direction, quantity));
-  _timeout.push_back(std::make_pair(_last_update_time + _config.order_timeout, order_id));
-  return true;
-}
-
-double OrderManager::get_exposure(quinclas::common::TradeDirection direction) const {
-  return 0.0;  // TODO(thraneh): implement
 }
 
 void OrderManager::check(const quinclas::common::MessageInfo& message_info) {
   auto current_time = message_info.enqueue_time;
-  LOG_IF(FATAL, _last_update_time < current_time) << "Detected wrong sequencing!";
+  LOG_IF(FATAL, _last_update_time < current_time) << "Wrong sequencing!";
   _last_update_time = current_time;
   // check timeout
   while (!_timeout.empty()) {
     const auto& front = _timeout.front();
     if (current_time < front.first)
       break;
-    auto order_id = front.second;
+    remove_order(front.second);
     _timeout.pop_front();
-    auto iter = _orders.find(order_id);
-    if (iter == _orders.end())
-      return;
-    LOG(WARNING) << "Order (id: " << order_id << ") has timed out!";
-    _orders.erase(iter);
   }
 }
 
--- examples/reference/order_manager.h
+++ examples/reference/order_manager.h
@@ -19,7 +19,12 @@ class OrderManager final {
   OrderManager(const Config& config, const RiskManager& risk_manager,
                quinclas::common::Strategy::Dispatcher& dispatcher);
 
- public:
+  // create order (convenience)
+  uint32_t buy(const char *order_template, double quantity,
+               double limit_price);
+  uint32_t sell(const char *order_template, double quantity,
+                double limit_price);
+
   // event handlers
   void on(const quinclas::common::TimerEvent& event);
   void on(const quinclas::common::CreateOrderAckEvent& event);
@@ -27,37 +32,35 @@ class OrderManager final {
   void on(const quinclas::common::CancelOrderAckEvent& event);
   void on(const quinclas::common::OrderUpdateEvent& order_update);
 
- public:
-  // create order (easy version)
-  bool buy_ioc(double quantity, double limit_price);
-  bool sell_ioc(double quantity, double limit_price);
-
  private:
-  // create order (internal version)
-  bool create_order(const char *order_template,
-                    quinclas::common::TradeDirection direction,
-                    double quantity, double limit_price);
-
- private:
-  // utilities
+  // order management
+  uint32_t create_order(const char *order_template,
+                        quinclas::common::TradeDirection direction,
+                        double quantity, double limit_price);
+  void update_order(uint32_t order_id, uint32_t gateway_order_id,
+                    double remaining_quantity);
+  void remove_order(uint32_t order_id);
+  // exposure
   double get_exposure(quinclas::common::TradeDirection direction) const;
+  void update_exposure(quinclas::common::TradeDirection direction,
+                       double delta_quantity);
+  void compute_and_update_exposure();
+  // consistency check
+  void check(const quinclas::common::MessageInfo& message_info);
 
  private:
   const Config& _config;
   const RiskManager& _risk_manager;
   quinclas::common::Strategy::Dispatcher& _dispatcher;
-
- private:
   // order management
-  uint32_t _next_internal_id = 0;
-  std::unordered_map<uint32_t, Order> _orders;
   typedef std::chrono::system_clock::time_point time_point_t;
+  uint32_t _next_order_id = 0;
+  std::unordered_map<uint32_t, Order> _orders;
   std::list<std::pair<time_point_t, uint32_t> > _timeout;
-
- private:
-  // consistency (particularly useful for simulation)
+  double _buy_exposure = 0.0;
+  double _sell_exposure = 0.0;
+  // consistency check
   time_point_t _last_update_time;
-  void check(const quinclas::common::MessageInfo& message_info);
 };
 
 }  // namespace reference

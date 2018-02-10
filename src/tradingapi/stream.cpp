aece628c5ed4fa041440a5c98a79f8a9af07533c
thraneh <thraneh@gmail.com>
2018-02-10 07:55:38 +0000

    moving output operators into namespace

--- src/tradingapi/stream.cpp
+++ src/tradingapi/stream.cpp
@@ -7,76 +7,63 @@
 
 #include "quinclas/tradingapi.h"
 
-#define _USE_DOUBLE_CONVERSION 1
-
 namespace quinclas {
 namespace common {
 
-}  // namespace common
-}  // namespace quinclas
-
 namespace {
-#if defined(_USE_DOUBLE_CONVERSION)
 const double_conversion::DoubleToStringConverter DOUBLE_CONVERTER(
     double_conversion::DoubleToStringConverter::UNIQUE_ZERO,
     "", "", 'e', -6, 21, 6, 0
 );
-#endif
 const char *TIME_FORMAT = "%E4Y-%m-%dT%H:%M:%E6S";
 const auto TIME_ZONE = cctz::utc_time_zone();
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::Number number) {
-#if defined(_USE_DOUBLE_CONVERSION)
+std::ostream& operator<<(std::ostream& stream, const Number number) {
   char buffer[32];
   double_conversion::StringBuilder builder(buffer, sizeof(buffer));
   if (DOUBLE_CONVERTER.ToShortest(number._value, &builder))
     return stream << builder.Finalize();
   else
     return stream << "####";
-#else
-  if (std::isfinite(number._value))
-    return stream << number._value;
-  return stream;
-#endif
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ConnectionStatus value) {
-  return stream << quinclas::common::EnumNameConnectionStatus(value);
+std::ostream& operator<<(std::ostream& stream, const ConnectionStatus value) {
+  return stream << EnumNameConnectionStatus(value);
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::LoginStatus value) {
-  return stream << quinclas::common::EnumNameLoginStatus(value);
+std::ostream& operator<<(std::ostream& stream, const LoginStatus value) {
+  return stream << EnumNameLoginStatus(value);
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::TradeDirection value) {
-  return stream << quinclas::common::EnumNameTradeDirection(value);
+std::ostream& operator<<(std::ostream& stream, const TradeDirection value) {
+  return stream << EnumNameTradeDirection(value);
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::TradingStatus value) {
-  return stream << quinclas::common::EnumNameTradingStatus(value);
+std::ostream& operator<<(std::ostream& stream, const TradingStatus value) {
+  return stream << EnumNameTradingStatus(value);
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::OrderStatus value) {
-  return stream << quinclas::common::EnumNameOrderStatus(value);
+std::ostream& operator<<(std::ostream& stream, const OrderStatus value) {
+  return stream << EnumNameOrderStatus(value);
 }
 
-std::ostream& operator<<(std::ostream& stream, quinclas::common::time_point_t time_point) {
+std::ostream& operator<<(std::ostream& stream, time_point_t time_point) {
   if (0 == time_point.time_since_epoch().count())
     return stream;
   return stream << cctz::format(TIME_FORMAT, time_point, TIME_ZONE);
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::Layer& value) {
+std::ostream& operator<<(std::ostream& stream, const Layer& value) {
   return stream << "{"
-    "bid_price=" << quinclas::common::Number(value.bid_price) << ", "
-    "bid_quantity=" << quinclas::common::Number(value.bid_quantity) << ", "
-    "ask_price=" << quinclas::common::Number(value.ask_price) << ", "
-    "ask_quantity=" << quinclas::common::Number(value.ask_quantity) <<
+    "bid_price=" << Number(value.bid_price) << ", "
+    "bid_quantity=" << Number(value.bid_quantity) << ", "
+    "ask_price=" << Number(value.ask_price) << ", "
+    "ask_quantity=" << Number(value.ask_quantity) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::GatewayStatus& value) {
+std::ostream& operator<<(std::ostream& stream, const GatewayStatus& value) {
   return stream << "{"
     "market_data_connection_status=" << value.market_data_connection_status << ", "
     "market_data_login_status=" << value.market_data_login_status << ", "
@@ -85,18 +72,18 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::GatewaySt
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ReferenceData& value) {
+std::ostream& operator<<(std::ostream& stream, const ReferenceData& value) {
   return stream << "{"
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.exchange << "\", "
-    "tick_size=" << quinclas::common::Number(value.tick_size) << ", "
-    "tick_size=" << quinclas::common::Number(value.tick_size) << ", "
-    "limit_up=" << quinclas::common::Number(value.limit_up) << ", "
-    "limit_down=" << quinclas::common::Number(value.limit_down) <<
+    "tick_size=" << Number(value.tick_size) << ", "
+    "tick_size=" << Number(value.tick_size) << ", "
+    "limit_up=" << Number(value.limit_up) << ", "
+    "limit_down=" << Number(value.limit_down) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::MarketStatus& value) {
+std::ostream& operator<<(std::ostream& stream, const MarketStatus& value) {
   return stream << "{"
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.exchange << "\", "
@@ -104,7 +91,7 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::MarketSta
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::MarketByPrice& value) {
+std::ostream& operator<<(std::ostream& stream, const MarketByPrice& value) {
   stream << "{"
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.instrument << "\", "
@@ -118,27 +105,27 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::MarketByP
   return stream << "]";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::SessionStatistics& value) {
+std::ostream& operator<<(std::ostream& stream, const SessionStatistics& value) {
   return stream << "{"
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.instrument << "\", "
-    "open=" << quinclas::common::Number(value.open) << ", "
-    "high=" << quinclas::common::Number(value.high) << ", "
-    "low=" << quinclas::common::Number(value.low) <<
+    "open=" << Number(value.open) << ", "
+    "high=" << Number(value.high) << ", "
+    "low=" << Number(value.low) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::DailyStatistics& value) {
+std::ostream& operator<<(std::ostream& stream, const DailyStatistics& value) {
   return stream << "{"
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.instrument << "\", "
-    "settlement=" << quinclas::common::Number(value.settlement) << ", "
-    "open_interest=" << quinclas::common::Number(value.open_interest) << ", "
-    "volume=" << quinclas::common::Number(value.volume) <<
+    "settlement=" << Number(value.settlement) << ", "
+    "open_interest=" << Number(value.open_interest) << ", "
+    "volume=" << Number(value.volume) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::OrderUpdate& value) {
+std::ostream& operator<<(std::ostream& stream, const OrderUpdate& value) {
   return stream << "{"
     "opaque=" << value.opaque << ", "
     "order_id=" << value.order_id << ", "
@@ -148,14 +135,14 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::OrderUpda
     "instrument=\"" << value.instrument << "\", "
     "status=" << value.status << ", "
     "trade_direction=" << value.trade_direction << ", "
-    "remaining_quantity=" << quinclas::common::Number(value.remaining_quantity) << ", "
-    "traded_quantity=" << quinclas::common::Number(value.traded_quantity) << ", "
+    "remaining_quantity=" << Number(value.remaining_quantity) << ", "
+    "traded_quantity=" << Number(value.traded_quantity) << ", "
     "insert_time=" << value.insert_time << ", "
     "cancel_time=" << value.cancel_time <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::TradeUpdate& value) {
+std::ostream& operator<<(std::ostream& stream, const TradeUpdate& value) {
   return stream << "{"
     "order_id=" << value.order_id << ", "
     "external_order_id=\"" << value.external_order_id << "\", "
@@ -163,13 +150,13 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::TradeUpda
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.instrument << "\", "
     "trade_direction=" << value.trade_direction << ", "
-    "quantity=" << quinclas::common::Number(value.quantity) << ", "
-    "price=" << quinclas::common::Number(value.price) << ", "
+    "quantity=" << Number(value.quantity) << ", "
+    "price=" << Number(value.price) << ", "
     "trade_time=" << value.trade_time <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CreateOrderAck& value) {
+std::ostream& operator<<(std::ostream& stream, const CreateOrderAck& value) {
   return stream << "{"
     "opaque=" << value.opaque << ", "
     "order_id=" << value.order_id << ", "
@@ -182,7 +169,7 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::CreateOrd
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ModifyOrderAck& value) {
+std::ostream& operator<<(std::ostream& stream, const ModifyOrderAck& value) {
   return stream << "{"
     "opaque=" << value.opaque << ", "
     "order_id=" << value.order_id << ", "
@@ -192,12 +179,12 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::ModifyOrd
     "external_order_id=\"" << value.external_order_id << "\", "
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.instrument << "\", "
-    "quantity_change=" << quinclas::common::Number(value.quantity_change) << ", "
-    "limit_price=" << quinclas::common::Number(value.limit_price) <<
+    "quantity_change=" << Number(value.quantity_change) << ", "
+    "limit_price=" << Number(value.limit_price) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CancelOrderAck& value) {
+std::ostream& operator<<(std::ostream& stream, const CancelOrderAck& value) {
   return stream << "{"
     "opaque=" << value.opaque << ", "
     "order_id=" << value.order_id << ", "
@@ -210,7 +197,7 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::CancelOrd
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::MessageInfo& value) {
+std::ostream& operator<<(std::ostream& stream, const MessageInfo& value) {
   return stream << "{"
     "gateway=\"" << value.gateway << "\", "
     "message_id=" << value.message_id << ", "
@@ -220,111 +207,111 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::MessageIn
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::GatewayStatusEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const GatewayStatusEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "gateway_status=" << value.gateway_status <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ReferenceDataEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const ReferenceDataEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "reference_data=" << value.reference_data <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::MarketStatusEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const MarketStatusEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "market_status=" << value.market_status <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::MarketByPriceEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const MarketByPriceEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "market_by_price=" << value.market_by_price <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::SessionStatisticsEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const SessionStatisticsEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "session_statistics=" << value.session_statistics <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::DailyStatisticsEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const DailyStatisticsEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "daily_statistics=" << value.daily_statistics <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::OrderUpdateEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const OrderUpdateEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "order_update=" << value.order_update <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::TradeUpdateEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const TradeUpdateEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "trade_update=" << value.trade_update <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CreateOrderAckEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const CreateOrderAckEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "create_order_ack=" << value.create_order_ack <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ModifyOrderAckEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const ModifyOrderAckEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "modify_order_ack=" << value.modify_order_ack <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CancelOrderAckEvent& value) {
+std::ostream& operator<<(std::ostream& stream, const CancelOrderAckEvent& value) {
   return stream << "{"
     "message_info=" << value.message_info << ", "
     "cancel_order_ack=" << value.cancel_order_ack <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CreateOrder& value) {
+std::ostream& operator<<(std::ostream& stream, const CreateOrder& value) {
   return stream << "{"
     "opaque=" << value.opaque << ", "
     "order_template=" << value.order_template << ", "
     "exchange=\"" << value.exchange << "\", "
     "instrument=\"" << value.instrument << "\", "
     "direction=" << value.direction << ", "
-    "quantity=" << quinclas::common::Number(value.quantity) << ", "
-    "limit_price=" << quinclas::common::Number(value.limit_price) << ", "
-    "stop_price=" << quinclas::common::Number(value.stop_price) <<
+    "quantity=" << Number(value.quantity) << ", "
+    "limit_price=" << Number(value.limit_price) << ", "
+    "stop_price=" << Number(value.stop_price) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ModifyOrder& value) {
+std::ostream& operator<<(std::ostream& stream, const ModifyOrder& value) {
   return stream << "{"
     "order_id=" << value.order_id <<
-    "quantity_change=" << quinclas::common::Number(value.quantity_change) << ", "
-    "limit_price=" << quinclas::common::Number(value.limit_price) <<
+    "quantity_change=" << Number(value.quantity_change) << ", "
+    "limit_price=" << Number(value.limit_price) <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CancelOrder& value) {
+std::ostream& operator<<(std::ostream& stream, const CancelOrder& value) {
   return stream << "{"
     "order_id=" << value.order_id <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::RequestInfo& value) {
+std::ostream& operator<<(std::ostream& stream, const RequestInfo& value) {
   return stream << "{"
     "destination=\"" << value.destination << "\", "
     "trace_source=\"" << value.trace_source << "\", "
@@ -334,23 +321,26 @@ std::ostream& operator<<(std::ostream& stream, const quinclas::common::RequestIn
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CreateOrderRequest& value) {
+std::ostream& operator<<(std::ostream& stream, const CreateOrderRequest& value) {
   return stream << "{"
     "request_info=" << value.request_info << ", "
     "create_order=" << value.create_order <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::ModifyOrderRequest& value) {
+std::ostream& operator<<(std::ostream& stream, const ModifyOrderRequest& value) {
   return stream << "{"
     "request_info=" << value.request_info << ", "
     "modify_order=" << value.modify_order <<
     "}";
 }
 
-std::ostream& operator<<(std::ostream& stream, const quinclas::common::CancelOrderRequest& value) {
+std::ostream& operator<<(std::ostream& stream, const CancelOrderRequest& value) {
   return stream << "{"
     "request_info=" << value.request_info << ", "
     "cancel_order=" << value.cancel_order <<
     "}";
 }
+
+}  // namespace common
+}  // namespace quinclas

ce52d1a056310409a6b53c2de6be26a8de555f2b
thraneh <thraneh@gmail.com>
2018-02-21 08:45:18 +0000

    libunwind has different implementation on linux and osx

--- src/tradingapi/logging.cpp
+++ src/tradingapi/logging.cpp
@@ -110,7 +110,11 @@ static void print_stacktrace(int signal, siginfo_t *info) {
       if (status == 0)
         name = demangled_name;
     }
+#if defined(__linux__)
     fprintf(stderr, "[%2d] %#*lx %s\n", index, width, ip, name);
+#elif defined(__APPLE__)
+    fprintf(stderr, "[%2d] %#*llx %s\n", index, width, ip, name);
+#endif
     if (demangled_name)
       free(demangled_name);
   }

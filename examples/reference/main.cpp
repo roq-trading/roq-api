212115b50d8a2c5c6d8ea074d89d8ebe199173b9
thraneh <thraneh@gmail.com>
2018-03-05 09:36:45 +0000

    more readable main function

--- examples/reference/main.cpp
+++ examples/reference/main.cpp
@@ -12,20 +12,14 @@
 
 using namespace examples::reference;  // NOLINT
 
-// common
+DEFINE_string(config_file, "",
+    "Config filename for strategy (path)");
 
 DEFINE_string(mode, "",
     "Mode of operation (trading|simulation)");
 
-DEFINE_string(config_file, "",
-    "Config filename for strategy (path)");
-
-// mode: trading
-
 DEFINE_string(local_address, "",
     "Host-internal (aka unix domain) socket address (path)");
-
-// mode: simulation
 
 DEFINE_string(simulation_file, "",
     "Simulation file (path)");
@@ -33,44 +27,47 @@ DEFINE_string(simulation_file, "",
 const char *GATEWAY = "FEMAS";
 
 int main(int argc, char *argv[]) {
-  // Initialize logging framework
   quinclas::logging::Logger::initialize(true);
 
-  // Parse command-line options.
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   gflags::ShutDownCommandLineFlags();
 
-  // Config file
   if (FLAGS_config_file.empty()) {
     LOG(ERROR) << "Missing parameter: --config-file";
     std::exit(EXIT_FAILURE);
   }
+
   auto config = ConfigReader(FLAGS_config_file).parse();
+
   LOG(INFO) << config;
 
-  if (FLAGS_mode == "trading") {  // trading
-    // Gateway connections
+  if (FLAGS_mode == "trading") {
+
     if (FLAGS_local_address.empty()) {
       LOG(ERROR) << "Missing parameter: --local-address";
       std::exit(EXIT_FAILURE);
     }
+
     std::unordered_map<std::string, std::string> gateways;
     gateways.emplace(GATEWAY, FLAGS_local_address);
-    // Create trading engine, strategy, then dispatch
+
     quinclas::client::Controller<GatewayManager>(
         std::move(gateways)).create_and_dispatch(std::move(config));
-  } else if (FLAGS_mode == "simulation") {  // simulation
-    // Simulation files
+
+  } else if (FLAGS_mode == "simulation") {
+
     if (FLAGS_simulation_file.empty()) {
       LOG(ERROR) << "Missing parameter: --simulation-file";
       std::exit(EXIT_FAILURE);
     }
+
     std::list<std::unique_ptr<quinclas::simulation::Generator> > generators;
     generators.emplace_back(new Generator(FLAGS_simulation_file));
-    // Create simulation engine, strategy, then dispatch
+
     quinclas::simulation::Controller<GatewayManager>(
         std::move(generators)).create_and_dispatch(std::move(config));
-  } else {  // invalid
+
+  } else {
     LOG(ERROR) << "Invalid parameter: mode=\"" << FLAGS_mode << "\"";
     std::exit(EXIT_FAILURE);
   }

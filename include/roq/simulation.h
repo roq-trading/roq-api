faa981f7c4c739342e57180c5e4ce0f36a759cd7
thraneh <thraneh@gmail.com>
2018-05-22 08:43:35 +0000

    WIP -- extending the simulator to allow user-supplied matching engine

--- include/roq/simulation.h
+++ include/roq/simulation.h
@@ -16,36 +16,259 @@ namespace simulation {
 // Generator
 class Generator {
  public:
+  class Dispatcher {
+   public:
+    virtual void on(const BatchBeginEvent& event) = 0;
+    virtual void on(const BatchEndEvent& event) = 0;
+    virtual void on(const MarketByPriceEvent& event) = 0;
+    virtual void on(const TradeSummaryEvent& event) = 0;
+  };
   Generator() {}
   virtual ~Generator() {}
   virtual std::pair<bool, std::chrono::system_clock::time_point> fetch() = 0;
-  virtual void dispatch(Strategy& strategy) = 0;
+  virtual void dispatch(Dispatcher& dispatcher) = 0;
 
  private:
   Generator(const Generator&) = delete;
   void operator=(const Generator&) = delete;
 };
 
+// Matcher
+class Matcher {
+ public:
+  class Dispatcher {
+   public:
+    virtual void on(const CreateOrderAckEvent&) = 0;
+    virtual void on(const ModifyOrderAckEvent&) = 0;
+    virtual void on(const CancelOrderAckEvent&) = 0;
+    virtual void on(const OrderUpdateEvent&) = 0;
+    virtual void on(const TradeUpdateEvent&) = 0;
+  };
+  Matcher(Dispatcher& dispatcher, const std::string& name)
+      : _dispatcher(dispatcher),
+        _name(name) {
+  }
+  virtual ~Matcher() {}
+  // TODO(thraneh): add timer + next_update
+  virtual void on(const BatchBeginEvent& event) = 0;
+  virtual void on(const BatchEndEvent& event) = 0;
+  virtual void on(const MarketByPriceEvent& event) = 0;
+  virtual void on(const TradeSummaryEvent& event) = 0;
+  virtual void on(const CreateOrder& create_order) = 0;
+  virtual void on(const ModifyOrder& modify_order) = 0;
+  virtual void on(const CancelOrder& cancel_order) = 0;
+
+ private:
+  Matcher(const Matcher&) = delete;
+  void operator=(const Matcher&) = delete;
+
+ protected:
+  Dispatcher& _dispatcher;
+  const std::string _name;
+
+ protected:
+  void send(const CreateOrderAck& create_order_ack) {
+    auto message_info = MessageInfo {
+      .source = _name.c_str()
+    };
+    auto event = CreateOrderAckEvent {
+      .message_info = message_info,
+      .create_order_ack = create_order_ack
+    };
+    _dispatcher.on(event);
+  }
+  void send(const ModifyOrderAck& modify_order_ack) {
+    auto message_info = MessageInfo {
+      .source = _name.c_str()
+    };
+    auto event = ModifyOrderAckEvent {
+      .message_info = message_info,
+      .modify_order_ack = modify_order_ack
+    };
+    _dispatcher.on(event);
+  }
+  void send(const CancelOrderAck& cancel_order_ack) {
+    auto message_info = MessageInfo {
+      .source = _name.c_str()
+    };
+    auto event = CancelOrderAckEvent {
+      .message_info = message_info,
+      .cancel_order_ack = cancel_order_ack
+    };
+    _dispatcher.on(event);
+  }
+  void send(const OrderUpdate& order_update) {
+    auto message_info = MessageInfo {
+      .source = _name.c_str()
+    };
+    auto event = OrderUpdateEvent {
+      .message_info = message_info,
+      .order_update = order_update
+    };
+    _dispatcher.on(event);
+  }
+  void send(const TradeUpdate& trade_update) {
+    auto message_info = MessageInfo {
+      .source = _name.c_str()
+    };
+    auto event = TradeUpdateEvent {
+      .message_info = message_info,
+      .trade_update = trade_update
+    };
+    _dispatcher.on(event);
+  }
+};
+
+// NoMatcher
+class NoMatcher final : public Matcher {
+ public:
+  NoMatcher(
+      Matcher::Dispatcher& dispatcher,
+      const std::string& name)
+      : Matcher(dispatcher, name) {
+  }
+  void on(const BatchBeginEvent& event) override {
+  }
+  void on(const BatchEndEvent& event) override {
+  }
+  void on(const MarketByPriceEvent& event) override {
+  }
+  void on(const TradeSummaryEvent& event) override {
+  }
+  void on(const CreateOrder& create_order) override {
+    throw std::runtime_error("Unexpected");
+  }
+  void on(const ModifyOrder& modify_order) override {
+    throw std::runtime_error("Unexpected");
+  }
+  void on(const CancelOrder& cancel_order) override {
+    throw std::runtime_error("Unexpected");
+  }
+};
+
+// SimpleMatcher
+class SimpleMatcher final : public Matcher {
+ public:
+  SimpleMatcher(
+      Matcher::Dispatcher& dispatcher,
+      const std::string& name)
+      : Matcher(dispatcher, name) {
+    if (name.empty())
+      throw std::runtime_error("Gateway name is required");
+  }
+  void on(const BatchBeginEvent& event) override {
+  }
+  void on(const BatchEndEvent& event) override {
+  }
+  void on(const MarketByPriceEvent& event) override {
+  }
+  void on(const TradeSummaryEvent& event) override {
+  }
+  void on(const CreateOrder& create_order) override {
+    auto create_order_ack = CreateOrderAck {
+      .account = create_order.account,
+      .order_id = create_order.order_id,
+      .failure = false,
+      .reason = "",
+      .order_local_id = 0,
+      .order_external_id = "",
+    };
+    send(create_order_ack);
+    auto order_update = OrderUpdate {
+      .account = create_order.account,
+      .order_id = create_order.order_id,
+      .exchange = create_order.exchange,
+      .symbol = create_order.symbol,
+      .order_status = OrderStatus::Completed,
+      .side = create_order.side,
+      .remaining_quantity = 0.0,
+      .traded_quantity = create_order.quantity,
+      .position_effect = create_order.position_effect,
+      .order_template = create_order.order_template,
+      .insert_time = std::chrono::time_point_cast<duration_t>(
+          std::chrono::system_clock::time_point()),
+      .cancel_time = std::chrono::time_point_cast<duration_t>(
+          std::chrono::system_clock::time_point()),
+      .order_local_id = 0,
+      .order_external_id = "",
+    };
+    send(order_update);
+    auto trade_update = TradeUpdate {
+      .account = create_order.account,
+      .trade_id = create_order.order_id,
+      .order_id = create_order.order_id,
+      .exchange = create_order.exchange,
+      .symbol = create_order.symbol,
+      .side = create_order.side,
+      .quantity = 0.0,
+      .price = create_order.limit_price,
+      .position_effect = create_order.position_effect,
+      .order_template = create_order.order_template,
+      .trade_time = std::chrono::time_point_cast<duration_t>(
+          std::chrono::system_clock::time_point()),
+      .order_external_id = "",
+      .trade_external_id = "",
+    };
+    send(trade_update);
+  }
+  void on(const ModifyOrder& modify_order) override {
+    auto modify_order_ack = ModifyOrderAck {
+      .account = modify_order.account,
+      .order_id = modify_order.order_id,
+      .failure = true,
+      .reason = "NOT_SUPPORTED",
+      .order_local_id = 0,
+      .order_external_id = "",
+    };
+    send(modify_order_ack);
+  }
+  void on(const CancelOrder& cancel_order) override {
+    auto cancel_order_ack = CancelOrderAck {
+      .account = cancel_order.account,
+      .order_id = cancel_order.order_id,
+      .failure = true,
+      .reason = "NOT_SUPPORTED",
+      .order_local_id = 0,
+      .order_external_id = "",
+    };
+    send(cancel_order_ack);
+  }
+};
+
 // Controller
-template <typename T>
+template <typename T, typename M>
 class Controller final {
   typedef std::list<std::unique_ptr<Generator> > generators_t;
 
  public:
-  explicit Controller(generators_t&& generators) : _generators(std::move(generators)) {}
+  explicit Controller(
+      generators_t&& generators,
+      const std::string& gateway = "")
+      : _generators(std::move(generators)),
+        _gateway(gateway) {
+  }
   template <typename... Args>
   void create_and_dispatch(Args&&... args) {
-    Dispatcher(_generators, std::forward<Args>(args)...).dispatch();
+    Dispatcher(
+      _generators,
+      _gateway,
+      std::forward<Args>(args)...).dispatch();
   }
 
  private:
   // Dispatcher
   class Dispatcher final
-      : public Strategy::Dispatcher {
+      : public Strategy::Dispatcher,
+        public Generator::Dispatcher,
+        public Matcher::Dispatcher {
    public:
     template <typename... Args>
-    explicit Dispatcher(generators_t& generators, Args&&... args)
+    explicit Dispatcher(
+        generators_t& generators,
+        const std::string& gateway,
+        Args&&... args)
         : _generators(generators),
+          _matcher(*this, gateway),
           _strategy(*this, std::forward<Args>(args)...) {}
     void dispatch() {
       initialize();
@@ -54,7 +277,7 @@ class Controller final {
         while (true) {
           auto res = iter->fetch();
           if (res.first) {
-            iter->dispatch(_strategy);
+            iter->dispatch(*this);
           } else {
             return;
           }
@@ -85,162 +308,106 @@ class Controller final {
     }
     void download_begin_event(const char *gateway) {
       auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      DownloadBegin download_begin {};
-      strategy.on(DownloadBeginEvent {
-          .message_info = message_info,
-          .download_begin = download_begin
-          });
+      auto message_info = MessageInfo {
+        .source = gateway
+      };
+      auto download_begin = DownloadBegin {};
+      auto event = DownloadBeginEvent {
+        .message_info = message_info,
+        .download_begin = download_begin
+      };
+      static_cast<Strategy&>(_strategy).on(event);
     }
     void download_end_event(const char *gateway) {
       auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      DownloadEnd download_end {};
-      strategy.on(DownloadEndEvent {
-          .message_info = message_info,
-          .download_end = download_end
-          });
+      auto message_info = MessageInfo {
+        .source = gateway
+      };
+      auto download_end = DownloadEnd {};
+      auto event = DownloadEndEvent {
+        .message_info = message_info,
+        .download_end = download_end
+      };
+      static_cast<Strategy&>(_strategy).on(event);
     }
     void gateway_status(const char *gateway, const char *name) {
       auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      MarketDataStatus market_data_status {
+      auto message_info = MessageInfo {
+        .source = gateway
+      };
+      auto market_data_status = MarketDataStatus {
         .status = GatewayStatus::Ready,
       };
-      strategy.on(MarketDataStatusEvent {
-          .message_info = message_info,
-          .market_data_status = market_data_status
-          });
+      auto event = MarketDataStatusEvent {
+        .message_info = message_info,
+        .market_data_status = market_data_status
+      };
+      static_cast<Strategy&>(_strategy).on(event);
     }
     void market_status(
         const char *gateway,
         const char *exchange,
         const char *symbol) {
       auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      MarketStatus market_status {
+      auto message_info = MessageInfo {
+        .source = gateway
+      };
+      auto market_status = MarketStatus {
         .exchange = exchange,
         .symbol = symbol,
         .trading_status = TradingStatus::Open,
       };
-      strategy.on(MarketStatusEvent {
-          .message_info = message_info,
-          .market_status = market_status
-          });
+      auto event = MarketStatusEvent {
+        .message_info = message_info,
+        .market_status = market_status
+      };
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+
+   private:
+    void on(const BatchBeginEvent& event) override {
+      _matcher.on(event);
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const BatchEndEvent& event) override {
+      _matcher.on(event);
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const MarketByPriceEvent& event) override {
+      _matcher.on(event);
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const TradeSummaryEvent& event) override {
+      _matcher.on(event);
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+
+   private:
+    void on(const CreateOrderAckEvent& event) override {
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const ModifyOrderAckEvent& event) override {
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const CancelOrderAckEvent& event) override {
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const OrderUpdateEvent& event) override {
+      static_cast<Strategy&>(_strategy).on(event);
+    }
+    void on(const TradeUpdateEvent& event) override {
+      static_cast<Strategy&>(_strategy).on(event);
     }
 
    private:
     void send(const CreateOrder& create_order, const std::string& gateway) override {
-      // FIXME(thraneh): validate + enqueue
-      create_order_ack(create_order, gateway.c_str());
-      order_update(create_order, gateway.c_str());
-      trade_update(create_order, gateway.c_str());
+      _matcher.on(create_order);
     }
     void send(const ModifyOrder& modify_order, const std::string& gateway) override {
-      // FIXME(thraneh): validate + enqueue
-      modify_order_ack(modify_order, gateway.c_str());
+      _matcher.on(modify_order);
     }
     void send(const CancelOrder& cancel_order, const std::string& gateway) override {
-      // FIXME(thraneh): validate + enqueue
-      cancel_order_ack(cancel_order, gateway.c_str());
-    }
-    void create_order_ack(const CreateOrder& create_order, const char *gateway) {
-      auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      CreateOrderAck create_order_ack {
-        .account = create_order.account,
-        .order_id = create_order.order_id,
-        .failure = false,
-        .reason = "",
-        .order_local_id = 0,
-        .order_external_id = "",
-      };
-      strategy.on(CreateOrderAckEvent {
-          .message_info = message_info,
-          .create_order_ack = create_order_ack
-          });
-    }
-    void modify_order_ack(const ModifyOrder& modify_order, const char *gateway) {
-      auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      ModifyOrderAck modify_order_ack {
-        .account = modify_order.account,
-        .order_id = modify_order.order_id,
-        .failure = true,
-        .reason = "NOT_SUPPORTED",
-        .order_local_id = 0,
-        .order_external_id = "",
-      };
-      strategy.on(ModifyOrderAckEvent {
-          .message_info = message_info,
-          .modify_order_ack = modify_order_ack
-          });
-    }
-    void cancel_order_ack(const CancelOrder& cancel_order, const char *gateway) {
-      auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      CancelOrderAck cancel_order_ack {
-        .account = cancel_order.account,
-        .order_id = cancel_order.order_id,
-        .failure = true,
-        .reason = "NOT_SUPPORTED",
-        .order_local_id = 0,
-        .order_external_id = "",
-      };
-      strategy.on(CancelOrderAckEvent {
-          .message_info = message_info,
-          .cancel_order_ack = cancel_order_ack
-          });
-    }
-    void order_update(const CreateOrder& create_order, const char *gateway) {
-      auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      OrderUpdate order_update {
-        .account = create_order.account,
-        .order_id = create_order.order_id,
-        .exchange = create_order.exchange,
-        .symbol = create_order.symbol,
-        .order_status = OrderStatus::Completed,
-        .side = create_order.side,
-        .remaining_quantity = 0.0,
-        .traded_quantity = create_order.quantity,
-        .position_effect = create_order.position_effect,
-        .order_template = create_order.order_template,
-        .insert_time = std::chrono::time_point_cast<duration_t>(
-            std::chrono::system_clock::time_point()),
-        .cancel_time = std::chrono::time_point_cast<duration_t>(
-            std::chrono::system_clock::time_point()),
-        .order_local_id = 0,
-        .order_external_id = "",
-      };
-      strategy.on(OrderUpdateEvent {
-          .message_info = message_info,
-          .order_update = order_update
-          });
-    }
-    void trade_update(const CreateOrder& create_order, const char *gateway) {
-      auto& strategy = static_cast<Strategy&>(_strategy);
-      MessageInfo message_info { .source = gateway };
-      TradeUpdate trade_update {
-        .account = create_order.account,
-        .trade_id = create_order.order_id,
-        .order_id = create_order.order_id,
-        .exchange = create_order.exchange,
-        .symbol = create_order.symbol,
-        .side = create_order.side,
-        .quantity = 0.0,
-        .price = create_order.limit_price,
-        .position_effect = create_order.position_effect,
-        .order_template = create_order.order_template,
-        .trade_time = std::chrono::time_point_cast<duration_t>(
-            std::chrono::system_clock::time_point()),
-        .order_external_id = "",
-        .trade_external_id = "",
-      };
-      strategy.on(TradeUpdateEvent {
-          .message_info = message_info,
-          .trade_update = trade_update
-          });
+      _matcher.on(cancel_order);
     }
 
    private:
@@ -251,6 +418,7 @@ class Controller final {
    private:
     T _strategy;
     generators_t& _generators;
+    M _matcher;
   };  // Dispatcher
 
  private:
@@ -260,6 +428,7 @@ class Controller final {
 
  private:
   generators_t _generators;
+  const std::string _gateway;
 };  // Controller
 
 }  // namespace simulation

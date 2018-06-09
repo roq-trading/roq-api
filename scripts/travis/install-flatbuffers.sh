a7f6c44fc0da90017f074a9de2893faeee2eb0c7
thraneh <thraneh@gmail.com>
2018-06-09 06:37:54 +0000

    using flatbuffers 1.9.0 for travis build

--- scripts/travis/install-flatbuffers.sh
+++ scripts/travis/install-flatbuffers.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -ex
-rm -rf flatbuffers-master.zip
-wget --content-disposition https://codeload.github.com/google/flatbuffers/zip/master
-unzip flatbuffers-master.zip
-cd flatbuffers-master
+# VERSION="master"
+VERSION="1.9.0"
+rm -rf "flatbuffers-$VERSION.zip"
+# wget --content-disposition https://codeload.github.com/google/flatbuffers/zip/master
+wget --content-disposition https://github.com/google/flatbuffers/archive/v1.9.0.zip
+unzip "flatbuffers-$VERSION.zip"
+cd "flatbuffers-$VERSION"
 cmake -DCMAKE_BUILD_TYPE=Release .
 make -j2
 sudo make install

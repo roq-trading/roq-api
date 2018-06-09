565b5078b57c95ff48b4e31365313d3fa8abb410
thraneh <thraneh@gmail.com>
2018-06-09 07:38:18 +0000

    fix getting username (fallback to using passwd)

--- include/roq/platform.h
+++ include/roq/platform.h
@@ -4,6 +4,11 @@
 
 #include <unistd.h>
 
+#if defined(__linux__)
+#include <sys/types.h>
+#include <pwd.h>
+#endif
+
 #if defined(__APPLE__)
 #include <mach-o/dyld.h>
 #include <libgen.h>
@@ -12,6 +17,7 @@
 
 #include <fstream>
 #include <string>
+#include <vector>
 
 namespace roq {
 namespace platform {
@@ -22,16 +28,24 @@ inline pid_t get_pid() {
 
 inline std::string get_hostname() {
   char buffer[256];  // gethostname(2)
-  if (gethostname(buffer, sizeof(buffer)) < 0)
-    return "<hostname>";
-  return buffer;
+  if (gethostname(buffer, sizeof(buffer)) >= 0)
+    return buffer;
+  return "<hostname?>";
 }
 
 inline std::string get_username() {
   char buffer[33];  // SF294121
-  if (getlogin_r(buffer, sizeof(buffer)) != 0)
-    return "<username>";
-  return buffer;
+  if (getlogin_r(buffer, sizeof(buffer)) == 0)
+    return buffer;
+#if defined(__linux__)
+  auto size = sysconf(_SC_GETPW_R_SIZE_MAX);
+  std::vector<char> buf(size > 0 ? size : 16384);
+  struct passwd pwd;
+  struct passwd *result;
+  if (getpwuid_r(getuid(), &pwd, &buf[0], buf.size(), &result) == 0)
+    return pwd.pw_name;
+#endif
+  return "<username?>";
 }
 
 // TODO(thraneh): cross-platform, e.g. SO1023306
@@ -46,8 +60,7 @@ inline std::string get_program() {
   uint32_t size = sizeof(buffer);
   if (_NSGetExecutablePath(buffer, &size) == 0)
     return basename(buffer);
-  else
-    return "<program>";
+  return "<program?>";
 #else
 #error "Don't know how to find program name"
 #endif

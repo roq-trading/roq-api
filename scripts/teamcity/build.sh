aad9c36d8840989cc3569a0a6c51f8368b459ddc
thraneh <thraneh@gmail.com>
2018-09-24 07:30:12 +0200

    adding build script for teamcity

---
+++ scripts/teamcity/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -ex
+
+curl -s https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > ./miniconda.sh
+bash ./miniconda.sh -b -p miniconda -u
+
+cat > miniconda/.condarc << EOF
+channels:
+  - anaconda
+  - https://roq-trading.com/conda/unstable
+EOF
+
+conda install -y \
+  autoconf \
+  automake \
+  libtool \
+  pkgconfig \
+  make \
+  cmake \
+  gcc_linux-64 \
+  gxx_linux-64 \
+  patchelf \
+  openssl \
+  gflags \
+  libevent \
+  roq-oss-benchmark \
+  roq-oss-cctz \
+  roq-oss-crossguid \
+  roq-oss-flatbuffers \
+  roq-oss-libucl \
+  roq-oss-libunwind \
+  roq-oss-spdlog
+
+source miniconda/bin/activate
+
+./autogen.sh
+./configure --prefix=$PREFIX
+make -j4

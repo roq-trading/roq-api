75778443407ed5d5ae53d934354f26479af2111f
thraneh <thraneh@gmail.com>
2018-10-06 10:08:50 +0200

    drop inline scripts

--- scripts/travis/install-crossguid.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-rm -rf crossguid-master.zip
-wget --content-disposition https://codeload.github.com/graeme-hill/crossguid/zip/master
-unzip crossguid-master.zip
-cd crossguid-master
-cmake -DCMAKE_BUILD_TYPE=Release .
-make -j2
-sudo make install

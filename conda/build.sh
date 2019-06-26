0e0325f5757c4b057aaa8628786e6182c997a7c7
thraneh <thraneh@gmail.com>
2019-06-26 15:56:50 +0000

    drop test (for now)

--- conda/build.sh
+++ conda/build.sh
@@ -11,12 +11,12 @@ export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 ./autogen.sh
 ./configure --prefix=$PREFIX --enable-doc
 make -j${CPU_COUNT:-2}
-set -e
-make check || EXIT_CODE=$?
-set +e
-cat test/roq-*.log
-if [ "$EXIT_CODE" -ne "0" ]; then
-	echo "*** FAILED TEST ***"
-	exit $EXIT_CODE
-fi
+# set -e
+# make check || EXIT_CODE=$?
+# set +e
+# cat test/roq-*.log
+# if [ "$EXIT_CODE" -ne "0" ]; then
+# 	echo "*** FAILED TEST ***"
+# 	exit $EXIT_CODE
+# fi
 make install-strip

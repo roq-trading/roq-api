c97a1c9897e9f1b8c37dae3f1367579c9f677511
thraneh <thraneh@gmail.com>
2020-07-15 11:46:10 +0200

    cmake support now mostly ready

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+# ROQ_USE_CMAKE=1
+
 env
+
+if [ -z ${ROQ_USE_CMAKE+x} ]; then
 
 export CPPFLAGS="-I$PREFIX/include -DNDEBUG -fvisibility=hidden $CPPFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
@@ -15,3 +19,19 @@ export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 make -j${CPU_COUNT:-2}
 
 make install-strip
+
+else
+
+cmake \
+  -DCMAKE_AR="${CMAKE_AR:-$AR}" \
+  -DCMAKE_RANLIB="${CMAKE_RANLIB:-$RANLIB}" \
+  -DCMAKE_NM="${CMAKE_NM:-$NM}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DBUILD_TESTING=ON \
+  .
+
+make VERBOSE=1 -j${CPU_COUNT:-2}
+make install
+
+fi

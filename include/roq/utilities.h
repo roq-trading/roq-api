73be1e7b3c39ce56be42d388153ba67f85bf3ced
thraneh <thraneh@gmail.com>
2020-11-03 06:21:59 +0100

    update .clang-format

--- include/roq/utilities.h
+++ include/roq/utilities.h
@@ -14,12 +14,9 @@ namespace roq {
 //! Extract price for \ref roq::Layer given \ref roq::Side.
 inline double price_from_side(const Layer &layer, Side side) {
   switch (side) {
-    case Side::BUY:
-      return layer.bid_price;
-    case Side::SELL:
-      return layer.ask_price;
-    default:
-      return std::numeric_limits<double>::quiet_NaN();
+    case Side::BUY: return layer.bid_price;
+    case Side::SELL: return layer.ask_price;
+    default: return std::numeric_limits<double>::quiet_NaN();
   }
 }
 
@@ -29,12 +26,10 @@ inline bool is_order_complete(const OrderStatus &status) {
     case OrderStatus::SENT:
     case OrderStatus::ACCEPTED:
     case OrderStatus::PENDING:
-    case OrderStatus::WORKING:
-      return false;
+    case OrderStatus::WORKING: return false;
     case OrderStatus::REJECTED:
     case OrderStatus::COMPLETED:
-    case OrderStatus::CANCELED:
-      return true;
+    case OrderStatus::CANCELED: return true;
     default:
       // XXX throw?
       return true;
@@ -44,12 +39,10 @@ inline bool is_order_complete(const OrderStatus &status) {
 //! Check if request has reached a final (completed) status
 inline bool is_request_complete(const RequestStatus &status) {
   switch (status) {
-    case RequestStatus::FORWARDED:
-      return false;
+    case RequestStatus::FORWARDED: return false;
     case RequestStatus::ACCEPTED:
     case RequestStatus::REJECTED:
-    case RequestStatus::TIMEOUT:
-      return true;
+    case RequestStatus::TIMEOUT: return true;
     default:
       // XXX throw?
       return true;
@@ -59,24 +52,18 @@ inline bool is_request_complete(const RequestStatus &status) {
 //! Get the opposite \ref Side.
 inline Side invert(Side side) {
   switch (side) {
-    case Side::BUY:
-      return Side::SELL;
-    case Side::SELL:
-      return Side::BUY;
-    default:
-      return side;
+    case Side::BUY: return Side::SELL;
+    case Side::SELL: return Side::BUY;
+    default: return side;
   }
 }
 
 //! Get notional sign given \ref roq::Side
 inline int sign(Side side) {
   switch (side) {
-    case Side::BUY:
-      return 1;
-    case Side::SELL:
-      return -1;
-    default:
-      return 0;
+    case Side::BUY: return 1;
+    case Side::SELL: return -1;
+    default: return 0;
   }
 }
 

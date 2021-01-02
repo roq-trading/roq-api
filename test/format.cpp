80a458775737ebc3737c461c2a8c2b3616918912
thraneh <thraneh@gmail.com>
2021-01-02 14:49:10 +0100

    support miniforge

--- test/format.cpp
+++ test/format.cpp
@@ -17,10 +17,11 @@ TEST(format, ConnectionStatus) {
 }
 
 TEST(format, subscribe) {
-  Subscribe subscribe{.accounts = {"test", "abc"},
-                      .symbols_by_exchange = {
-                          {"abc", {"123", "234"}},
-                      }};
+  Subscribe subscribe{
+      .accounts = {"test", "abc"},
+      .symbols_by_exchange = {
+          {"abc", {"123", "234"}},
+      }};
   auto result = fmt::format("{}", subscribe);
   EXPECT_GT(result.length(), size_t{0});
   EXPECT_EQ(
@@ -46,20 +47,21 @@ TEST(format, market_by_price) {
       {.price = 4.0, .quantity = 10.0},
       {.price = 5.0, .quantity = 12.0},
   };
-  roq::MarketByPriceUpdate market_by_price{.exchange = "deribit",
-                                           .symbol = "BTC-27DEC19",
-                                           .bids =
-                                               {
-                                                   .items = bids,
-                                                   .length = std::size(bids),
-                                               },
-                                           .asks =
-                                               {
-                                                   .items = asks,
-                                                   .length = std::size(asks),
-                                               },
-                                           .snapshot = true,
-                                           .exchange_time_utc = {}};
+  roq::MarketByPriceUpdate market_by_price{
+      .exchange = "deribit",
+      .symbol = "BTC-27DEC19",
+      .bids =
+          {
+              .items = bids,
+              .length = std::size(bids),
+          },
+      .asks =
+          {
+              .items = asks,
+              .length = std::size(asks),
+          },
+      .snapshot = true,
+      .exchange_time_utc = {}};
   auto result = fmt::format("{}", market_by_price);
   EXPECT_GT(result.length(), size_t{0});
   EXPECT_EQ(
@@ -67,8 +69,8 @@ TEST(format, market_by_price) {
       R"({)"
       R"(exchange="deribit", )"
       R"(symbol="BTC-27DEC19", )"
-      R"(bids=[{price=1.0, quantity=2.0}, {price=2.0, quantity=4.0}, {price=3.0, quantity=8.0}, {price=4.0, quantity=10.0}, {price=5.0, quantity=12.0}], )"
-      R"(asks=[{price=1.0, quantity=2.0}, {price=2.0, quantity=4.0}, {price=3.0, quantity=8.0}, {price=4.0, quantity=10.0}, {price=5.0, quantity=12.0}], )"
+      R"(bids=[{price=1, quantity=2}, {price=2, quantity=4}, {price=3, quantity=8}, {price=4, quantity=10}, {price=5, quantity=12}], )"
+      R"(asks=[{price=1, quantity=2}, {price=2, quantity=4}, {price=3, quantity=8}, {price=4, quantity=10}, {price=5, quantity=12}], )"
       R"(snapshot=true, )"
       R"(exchange_time_utc=0ns)"
       R"(})");

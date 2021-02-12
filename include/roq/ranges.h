9f847ece6ecb381f60097d227406cc2eee522a60
thraneh <thraneh@gmail.com>
2021-02-12 02:47:17 +0100

    wrapper for ranges-v3

--- include/roq/ranges.h
+++ include/roq/ranges.h
@@ -2,16 +2,18 @@
 
 #pragma once
 
-// objective: prepare for using <span> (since C++20)
+// objective: prepare for using <ranges> (since C++20)
 
-// https://github.com/martinmoene/span-lite
-#include <nonstd/span.hpp>
+// https://github.com/ericniebler/range-v3
+#include <range/v3/view.hpp>
 
 // namespace envelope so we can more easily manage and replace
 // dependencies when making the transition to C++20
 
 namespace roq {
+namespace ranges {
 
-using nonstd::span;
+using ::ranges::iota_view;
 
+}  // namespace ranges
 }  // namespace roq
---
+++ test/ranges.cpp
@@ -0,0 +1,14 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#include <gtest/gtest.h>
+
+#include "roq/ranges.h"
+
+using namespace roq;  // NOLINT
+
+TEST(ranges, iota_view) {
+  size_t sum = 0;
+  for (auto i : roq::ranges::iota_view(5, 10))
+    sum += i;
+  EXPECT_EQ(sum, 35);
+}

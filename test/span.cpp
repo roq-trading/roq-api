21bf010ad3bf64939dae387deb5dc6aefcf588a0
thraneh <thraneh@gmail.com>
2021-02-21 12:19:19 +0100

    revert to using span-lite

--- test/span.cpp
+++ test/span.cpp
@@ -10,7 +10,7 @@ using namespace roq::literals;
 
 TEST(span, simple) {
   std::array<double, 3> raw{0.0, 2.0, 3.0};
-  roq::span span(raw.data(), raw.size());
+  roq::span span(raw);
   span[0] = 1.0;
   EXPECT_DOUBLE_EQ(span[0], 1.0);
   EXPECT_DOUBLE_EQ(span[1], 2.0);
@@ -27,7 +27,7 @@ TEST(span, string_view) {
       "abc"_sv,
       "def"_sv,
   };
-  roq::span span(raw.data(), raw.size());
+  roq::span span(raw);
   EXPECT_EQ(span[0], "abc"_sv);
   EXPECT_EQ(span[1], "def"_sv);
 }

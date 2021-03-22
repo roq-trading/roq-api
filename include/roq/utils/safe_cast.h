d9516760ee3ce2016c65bdcb7d04c7e835536a1e
thraneh <thraneh@gmail.com>
2021-03-22 14:43:59 +0100

    safe_cast is a no-op when result type match value type

--- include/roq/utils/safe_cast.h
+++ include/roq/utils/safe_cast.h
@@ -22,6 +22,8 @@ constexpr std::false_type always_false{};
 
 template <typename T>
 struct safe_cast final {
+  using value_type = typename std::remove_reference<T>::type;
+
   explicit safe_cast(T value) : value_(value) {}
 
   safe_cast(safe_cast &&) = default;
@@ -29,30 +31,38 @@ struct safe_cast final {
 
   template <typename R>
   operator R() const {
-    if constexpr (is_integer<T>::value) {
-      if (!(value_ >= std::numeric_limits<R>::min() && value_ <= std::numeric_limits<R>::max()))
+    using result_type = typename std::remove_reference<R>::type;
+    if constexpr (std::is_same<result_type, value_type>::value) {
+      // same
+      return value_;
+    } else if constexpr (is_integer<value_type>::value) {
+      // integer to ...
+      if (!(value_ >= std::numeric_limits<result_type>::min() &&
+            value_ <= std::numeric_limits<result_type>::max()))
         throw std::overflow_error("overflow"_s);
-      return static_cast<R>(value_);
-    } else if constexpr (std::is_floating_point<T>::value) {
-      if constexpr (is_integer<R>::value && std::is_signed<R>::value) {
+      return static_cast<result_type>(value_);
+    } else if constexpr (std::is_floating_point<value_type>::value) {
+      // floating point to ...
+      if constexpr (is_integer<result_type>::value && std::is_signed<result_type>::value) {
+        // ... signed integer
         // references:
         //   https://stackoverflow.com/a/30424410
-        if (!(value_ > static_cast<double>(std::numeric_limits<R>::min()) &&
-              value_ < static_cast<double>(std::numeric_limits<R>::max())))
+        if (!(value_ > static_cast<double>(std::numeric_limits<result_type>::min()) &&
+              value_ < static_cast<double>(std::numeric_limits<result_type>::max())))
           throw std::overflow_error("overflow"_s);
-        return static_cast<R>(value_);
+        return static_cast<result_type>(value_);
       } else {
-        static_assert(detail::always_false<R>, "not implemented for unsigned");
+        static_assert(detail::always_false<result_type>, "not implemented for unsigned");
       }
-    } else if constexpr (is_duration<T>::value) {
-      return std::chrono::duration_cast<R>(value_);
+    } else if constexpr (is_duration<value_type>::value) {
+      return std::chrono::duration_cast<result_type>(value_);
     } else {
-      static_assert(detail::always_false<T>, "not implemented for this type");
+      static_assert(detail::always_false<result_type>, "not implemented for this type");
     }
   }
 
  private:
-  T value_;
+  value_type value_;
 };
 
 }  // namespace utils

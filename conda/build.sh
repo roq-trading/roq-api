7666cdb3a5bcc15ab451fa396be2baa7f34707bd
thraneh <thraneh@gmail.com>
2021-04-12 16:09:33 +0200

    update conda

--- conda/build.sh
+++ conda/build.sh
@@ -2,19 +2,25 @@
 
 set -e
 
-env | sort
-
 CXXFLAGS+=" $CPPFLAGS"  # CMake doesn't used CPPFLAGS
 CXXFLAGS+=" -Wno-error=unused-result"  # gtest doesn't check fwrite return value
 
+echo -e "\033[1;34m--- ENV ---\033[0m"
+
+env | sort
+
+echo -e "\033[1;34m--- CMAKE ---\033[0m"
+
 cmake \
-  -DCMAKE_AR="${CMAKE_AR:-$AR}" \
-  -DCMAKE_RANLIB="${CMAKE_RANLIB:-$RANLIB}" \
-  -DCMAKE_NM="${CMAKE_NM:-$NM}" \
+  ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
   -DBUILD_TESTING=ON \
   -DBUILD_DOCS=ON \
   .
 
+echo -e "\033[1;34m--- MAKE ---\033[0m"
+
 make VERBOSE=1 -j${CPU_COUNT:-2}
 make install
+
+echo -e "\033[1;34m--- DONE ---\033[0m"

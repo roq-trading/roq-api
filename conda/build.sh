119e18e0eb4486b6c609d7f4be63c9878d2a5a42
thraneh <thraneh@gmail.com>
2021-11-28 08:46:49 +0100

    release build hides symbols

--- conda/build.sh
+++ conda/build.sh
@@ -14,6 +14,7 @@ echo -e "\033[1;34m--- CMAKE ---\033[0m"
 cmake \
   ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_TESTING=ON \
   -DBUILD_DOCS=ON \
   .

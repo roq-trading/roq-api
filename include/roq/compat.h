44ae5083e9642bbf0752731d4c142c74a385a9a1
thraneh <thraneh@gmail.com>
2022-02-22 07:48:19 +0100

    using c++ attributes for better compatibility

--- include/roq/compat.h
+++ include/roq/compat.h
@@ -3,10 +3,10 @@
 #pragma once
 
 #if defined(__GNUC__) || defined(__clang__)
-#define ROQ_PUBLIC __attribute__((visibility("default")))
-#define ROQ_HOT __attribute__((hot))
-#define ROQ_PACKED __attribute__((__packed__))
-#else
+#define ROQ_PUBLIC [[gnu::visibility("default")]]
+#define ROQ_HOT [[gnu::hot]]
+#define ROQ_PACKED [[gnu::packed]]
+#else  // not gcc or clang
 #define ROQ_PUBLIC
 #define ROQ_HOT
 #define ROQ_PACKED
@@ -24,7 +24,11 @@
 #define ROQ_CACHELINE_SIZE 64
 #define ROQ_PAGE_SIZE 4096
 #endif
+
+// note!
+//
 // constexpr std::hardware_{constructive,destructive}_interference_size
+//
 // not supported by gcc:
-// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0154r1.html
-// https://gcc.gnu.org/onlinedocs/libstdc++/manual/status.html#status.iso.2017
+//   http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0154r1.html
+//   https://gcc.gnu.org/onlinedocs/libstdc++/manual/status.html#status.iso.2017

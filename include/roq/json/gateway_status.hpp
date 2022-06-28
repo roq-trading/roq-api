be2eca4ac38193ba83b5788097bf12a4c6518dd4
thraneh <thraneh@gmail.com>
2022-06-28 07:15:43 +0200

    more json + utils function to optionally get account/exchange/symbol

---
+++ include/roq/json/gateway_status.hpp
@@ -0,0 +1,54 @@
+/* Copyright (c) 2017-2022, Hans Erik Thrane */
+
+#pragma once
+
+#include <fmt/format.h>
+
+#include "roq/gateway_status.hpp"
+
+#include "roq/json/date.hpp"
+#include "roq/json/datetime.hpp"
+#include "roq/json/number.hpp"
+#include "roq/json/string.hpp"
+
+namespace roq {
+namespace json {
+
+struct GatewayStatus final {
+  explicit GatewayStatus(roq::GatewayStatus const &value) : value_(value) {}
+
+  template <typename Context>
+  auto format_to(Context &context) const {
+    using namespace std::literals;
+    return fmt::format_to(
+        context.out(),
+        R"({{)"
+        R"("account":{},)"
+        R"("supported":{},)"
+        R"("available":{},)"
+        R"("unavailable":{})"
+        R"(}})"sv,
+        String{value_.account},
+        String{value_.supported},
+        String{value_.available},
+        String{value_.unavailable});
+  }
+
+ private:
+  roq::GatewayStatus const &value_;
+};
+
+}  // namespace json
+}  // namespace roq
+
+template <>
+struct fmt::formatter<roq::json::GatewayStatus> {
+  template <typename Context>
+  constexpr auto parse(Context &context) {
+    return std::begin(context);
+  }
+  template <typename Context>
+  auto format(roq::json::GatewayStatus const &value, Context &context) {
+    return value.format_to(context);
+  }
+};
---
+++ include/roq/utils/get.hpp
@@ -0,0 +1,62 @@
+/* Copyright (c) 2017-2022, Hans Erik Thrane */
+
+#pragma once
+
+#include <string_view>
+
+namespace roq {
+namespace utils {
+
+// account
+
+namespace detail {
+template <typename T, typename = void>
+struct has_account : std::false_type {};
+template <typename T>
+struct has_account<T, decltype((void)T::account, void())> : std::true_type {};
+}  // namespace detail
+
+template <typename T>
+std::string_view constexpr get_account(T const &value) {
+  if constexpr (detail::has_account<T>::value)
+    return value.account;
+  else
+    return {};
+}
+
+// exchange
+
+namespace detail {
+template <typename T, typename = void>
+struct has_exchange : std::false_type {};
+template <typename T>
+struct has_exchange<T, decltype((void)T::exchange, void())> : std::true_type {};
+}  // namespace detail
+
+template <typename T>
+std::string_view constexpr get_exchange(T const &value) {
+  if constexpr (detail::has_exchange<T>::value)
+    return value.exchange;
+  else
+    return {};
+}
+
+// symbol
+
+namespace detail {
+template <typename T, typename = void>
+struct has_symbol : std::false_type {};
+template <typename T>
+struct has_symbol<T, decltype((void)T::symbol, void())> : std::true_type {};
+}  // namespace detail
+
+template <typename T>
+std::string_view constexpr get_symbol(T const &value) {
+  if constexpr (detail::has_symbol<T>::value)
+    return value.symbol;
+  else
+    return {};
+}
+
+}  // namespace utils
+}  // namespace roq

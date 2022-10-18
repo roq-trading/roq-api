2e53b78b86078418a631af9d0b7352597a04d7d6
thraneh <thraneh@gmail.com>
2022-10-18 13:56:39 +0200

    note about clang15 and __builtin_source_location

--- include/roq/source_location.hpp
+++ include/roq/source_location.hpp
@@ -6,6 +6,8 @@
 // clang14 doesn't support std::source_location
 // partial support may potentially arrive with clang15
 // we will fail this #include for all other compilers
+// note!
+// clang15 only added __builtin_source_location
 #ifndef __clang__
 #error "should not be included"
 #endif
@@ -32,13 +34,20 @@ struct source_location final {
     return source_location(file, line, function);
   }
 
-  constexpr char const *file_name() const noexcept { return file_; }
-  constexpr std::uint_least32_t line() const noexcept { return line_; }
-  constexpr char const *function_name() const noexcept { return function_; }
+  constexpr char const *file_name() const noexcept {
+    return file_;
+  }
+  constexpr std::uint_least32_t line() const noexcept {
+    return line_;
+  }
+  constexpr char const *function_name() const noexcept {
+    return function_;
+  }
 
  protected:
   constexpr source_location(char const *file, std::int_least32_t line, char const *function)
-      : file_(file), line_(line), function_(function) {}
+      : file_(file), line_(line), function_(function) {
+  }
 
  private:
   char const *const file_;

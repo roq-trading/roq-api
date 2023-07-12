968db57a904d8d916c9837ba37f254dd9d51244b
thraneh <thraneh@gmail.com>
2023-07-12 07:38:31 +0200

    refactor flags and command-line parsing

---
+++ include/roq/args/parser.hpp
@@ -0,0 +1,24 @@
+/* Copyright (c) 2017-2023, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/compat.hpp"
+
+#include <span>
+#include <string_view>
+
+namespace roq {
+namespace args {
+
+struct ROQ_PUBLIC Parser {
+  virtual ~Parser() = default;
+
+  // note! c-style with first element being the program name
+  virtual operator std::span<std::string_view const>() const = 0;
+
+  inline auto program_name() const { return static_cast<std::span<std::string_view const>>(*this)[0]; }
+  inline auto params() const { return static_cast<std::span<std::string_view const>>(*this).subspan(1); }
+};
+
+}  // namespace args
+}  // namespace roq

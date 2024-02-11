d568339b8008be075a92308b19625a9d42ee8f53
thraneh <thraneh@gmail.com>
2024-02-11 12:27:43 +0100

    chore

--- include/roq/oms.hpp
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2017-2024, Hans Erik Thrane */
-
-#pragma once
-
-#include "roq/oms/exceptions.hpp"
-#include "roq/oms/order.hpp"
-#include "roq/oms/order_update.hpp"
-#include "roq/oms/response.hpp"

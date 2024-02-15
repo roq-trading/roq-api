5e9828ea9ad53d97017397f2214d16832425e2e4
thraneh <thraneh@gmail.com>
2024-02-15 16:03:08 +0100

    differentiate request count vs weight

--- include/roq/rate_limit_type.hpp
+++ include/roq/rate_limit_type.hpp
@@ -13,9 +13,10 @@ namespace roq {
 //! Rate limit type
 enum class RateLimitType : uint8_t {
   UNDEFINED = 0,
-  ORDER_ACTION,  //!< Any order action: create, modify, cancel, etc.
-  CREATE_ORDER,  //!< Only create order
-  REQUEST,       //!< Requests (possibly weighted)
+  ORDER_ACTION,    //!< Any order action: create, modify, cancel, etc.
+  CREATE_ORDER,    //!< Only create order
+  REQUEST,         //!< Requests (count)
+  REQUEST_WEIGHT,  //!< Requests (weighted)
 };
 
 }  // namespace roq

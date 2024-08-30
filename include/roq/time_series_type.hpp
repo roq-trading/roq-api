7adde61440fbef71eaca7abc7acc7040c6ba1b3f
thraneh <thraneh@gmail.com>
2024-08-30 15:56:01 +0200

    preparing to support time-series

---
+++ include/roq/time_series_type.hpp
@@ -0,0 +1,28 @@
+/* Copyright (c) 2017-2024, Hans Erik Thrane */
+
+/* !!! THIS FILE HAS BEEN AUTO-GENERATED !!! */
+
+#pragma once
+
+#include <magic_enum.hpp>
+
+#include <fmt/core.h>
+
+namespace roq {
+
+//! Enumeration of time-series types
+enum class TimeSeriesType : uint8_t {
+  UNDEFINED = 0,
+  TRADES,  //!< Trade summary
+};
+
+}  // namespace roq
+
+template <>
+struct fmt::formatter<roq::TimeSeriesType> {
+  constexpr auto parse(format_parse_context &context) { return std::begin(context); }
+  auto format(roq::TimeSeriesType const &value, format_context &context) const {
+    using namespace std::literals;
+    return fmt::format_to(context.out(), "{}"sv, magic_enum::enum_name(value));
+  }
+};

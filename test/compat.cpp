e74de3257129c8e12818a13878408a7bb7564b83
thraneh <thraneh@gmail.com>
2024-11-23 04:40:04 +0100

    fix clang

--- test/compat.cpp
+++ test/compat.cpp
@@ -4,10 +4,10 @@
 
 #if defined(__APPLE__)
 #if defined(__arm64__)
-#ifdef __cpp_lib_hardware_interference_size  // note! not supported by clang19
-static_assert(std::hardware_destructive_interference_size == 128);
-static_assert(std::hardware_constructive_interference_size == 128);
-#endif
+// #ifdef __cpp_lib_hardware_interference_size  // note! not supported by clang19
+// static_assert(std::hardware_destructive_interference_size == 128);
+// static_assert(std::hardware_constructive_interference_size == 128);
+// #endif
 #else  // not __arm64__
 static_assert(std::hardware_destructive_interference_size == 64);
 static_assert(std::hardware_constructive_interference_size == 64);

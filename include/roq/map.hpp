f01eccb087d68a98ac937c9ba67a9737c777cc83
thraneh <thraneh@gmail.com>
2025-04-04 05:22:21 +0200

    enum mapper

---
+++ include/roq/map.hpp
@@ -0,0 +1,57 @@
+/* Copyright (c) 2017-2025, Hans Erik Thrane */
+
+#pragma once
+
+#include <optional>
+#include <tuple>
+
+namespace roq {
+
+namespace detail {
+// note! constexpr helper for static testing
+template <typename... Args>
+struct MapHelper final {
+  constexpr explicit MapHelper(std::tuple<Args...> const &args) : args_{args} {}
+  constexpr explicit MapHelper(Args &&...args_) : args_{std::forward<Args>(args_)...} {}
+
+  template <typename R>
+  constexpr operator std::optional<R>() const;
+
+  template <typename R>
+  constexpr bool operator==(R rhs) const {
+    auto tmp = (*this).operator std::optional<R>();
+    if (tmp.has_value())
+      return tmp.value() == rhs;
+    return false;
+  }
+
+ private:
+  std::tuple<Args...> const args_;
+};
+}  // namespace detail
+
+template <typename... Args>
+struct Map final {
+  inline explicit Map(Args &&...args) : args_{std::forward<Args>(args)...} {}
+  inline explicit Map(Args const &...args) : args_{args...} {}
+
+  Map(Map const &) = delete;
+
+  template <typename R>
+  operator std::optional<R>();
+
+  template <typename R>
+  inline operator R() {
+    return (*this).operator std::optional<R>().value();
+  }
+
+ private:
+  std::tuple<Args...> const args_;
+};
+
+template <typename... Args>
+inline auto map(Args &&...args) {
+  return Map{std::forward<Args>(args)...};
+}
+
+}  // namespace roq

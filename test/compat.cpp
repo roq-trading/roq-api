77f73232c3e660f1323ed123629d12c1681a8e22
thraneh <thraneh@gmail.com>
2025-09-09 05:11:29 +0200

    fix test

--- test/compat.cpp
+++ test/compat.cpp
@@ -15,7 +15,7 @@ static_assert(std::hardware_constructive_interference_size == 64);
 static_assert(std::hardware_destructive_interference_size == 256);  // note! since gcc12
 static_assert(std::hardware_constructive_interference_size == 64);
 #else  // not __arm64__
-static_assert(std::hardware_destructive_interference_size == 64);
+static_assert(std::hardware_destructive_interference_size == 256);
 static_assert(std::hardware_constructive_interference_size == 64);
 #endif
 #endif

3a7c0250869bd01891eb9fb3544e1e7b37fc04bf
thraneh <thraneh@gmail.com>
2026-09-16 05:54:26 +0000

    chore

--- test/compat.cpp
+++ test/compat.cpp
@@ -12,7 +12,7 @@ static_assert(std::hardware_constructive_interference_size == 64);
 #endif
 #else  // not __APPLE__
 #if defined(__arm64__)
-static_assert(std::hardware_destructive_interference_size == 256);  // note! since gcc12
+static_assert(std::hardware_destructive_interference_size == 256);
 static_assert(std::hardware_constructive_interference_size == 64);
 #else  // not __arm64__
 // static_assert(std::hardware_destructive_interference_size == 64);
